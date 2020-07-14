4c54302eebe3aa19f142974b3858c654fe75d828
John Loper <john.and.loper@gmail.com>
2020-07-13 17:26:35 -0700

    added static frontend

---
+++ build/precache-manifest.7eeb0177b4b17c9863a74509f18e413c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb9f77cbbd1387c9abf1588047aec0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0394bdcb370f40f822cb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0394bdcb370f40f822cb",
+    "url": "/static/js/main.86f6456c.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
