04328050d74ee2b02793389605bfe6b6b0f58ff9
John Loper <john.and.loper@gmail.com>
2020-07-22 15:16:07 -0700

    3.18, backend and local frontend get/delete/put/info working

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// for .env file
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-// Person constructor module
-const Person = require('./modules/person')
+const Person = require('./modules/mongoosePerson')
+const { response } = require('express')
 
-// required for using react production build
 app.use(express.static('build'))
-
 app.use(express.json())
-
 app.use(cors())
+// app.use(logger)
 
 // this prints GET/POST etc. stuff to console
 morgan.token('post-body', (req, res) => { return JSON.stringify(req.body) })
@@ -22,34 +19,38 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
+// get all persons
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons)
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    // const id = Number(req.params.id)
-    // const person = persons.find(note => note.id === id)
-
-    // if (person) {
-    //     res.json(person)
-    // } else {
-    //     res.status(404).end()
-    // }
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    })
+// get specific person using id
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                next('Person not found')
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+// delete specific person using id
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(deletedPerson => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
+// add person
 app.post('/api/persons', (req, res) => {
+    console.log('post beginning')
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -68,14 +69,46 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
-app.get('/info', (req, res) => {
-    const phonebookLength = persons.length
-    const timeStamp = new Date()
-    res.send(`
-        <p>Phonebook has info for ${phonebookLength} people.
-        </p> <p>${timeStamp}</p>
-        `)
+// update person
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
+
+app.get('/info', (req, res) => {
+    Person.find({}).then(persons => {
+        const phonebookLength = persons.length
+        const timeStamp = new Date()
+        res.send(`
+            <p>Phonebook has info for ${phonebookLength} people.
+            </p> <p>${timeStamp}</p>
+            `)
+    })
+})
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    } else if (error === 'Person not found') {
+        res.status(404).send({ error: 'Person not found' })
+    }
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT , () => {
